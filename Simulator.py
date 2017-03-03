6239e936697625cd17abbf1b94530c667322a408
Mike Moore <mickety.mike@gmail.com>
2017-03-02 21:12:03 -0600

    Intermediate commit. Working on simulator. Broken.

--- Simulator.py
+++ Simulator.py
@@ -1,17 +1,54 @@
 import wb_comm_if_pb2
+import time
 
 class Simulator(object):
 	def __init__(self):
+		self.wayPointList = []
+		self.rotationalVel = 0.1
+		self.translationalVel = 0.1
+		self.heading = 0.0
+		self.distanceTraveled = 0.0
+		self.positionX = 0.0
+		self.positionY = 0.0
+		self.timeStep = 0.1
+		self.headingTolerance = 0.1
+		self.distanceTolerance = 0.1
 		return
 
+	def run():
+		if self.wayPointList:
+			activeWayPoint = self.wayPointList.pop()
+			rotateToWayPoint(activeWayPoint)
+			translateToWayPoint(activeWayPoint)
+
+	def rotateToWayPoint(self, way_point):
+		errorHeading = way_point.Heading - self.heading
+		while abs(errorHeading) > self.headingTolerance:
+			if errorHeading > 0:
+				self.heading += self.rotationalVel * self.timeStep
+			else:
+				self.heading -= self.rotationalVel * self.timeStep
+			errorHeading = way_point.Heading - self.heading
+			time.sleep(self.timeStep)
+
+	def translateToWayPoint(self, way_point):
+		errorDistance = way_point.Distance
+		while abs(errorDistance) > self.distanceTolerance:
+			if errorDistance > 0:
+				self.distanceTraveled += self.translationalVel * self.timeStep
+			else:
+				self.distanceTraveled -= self.translationalVel * self.timeStep
+			errorDistance = way_point.Distance - self.distanceTraveled
+			time.sleep(self.timeStep)
+
 	def addWayPoint(self, way_point):
-		return
+		self.wayPointList.append(way_point)
 
 	def getSimData(self):
 		wb_data = wb_comm_if_pb2.WheelBotTelemetry()
 		wb_data.ObstacleDistance = 2.5
-		wb_data.Heading = 35.0
-		wb_data.DriveDistanceEstimate = 1.5
+		wb_data.Heading = self.heading
+		wb_data.DriveDistanceEstimate = self.distanceTraveled
 		wb_data.WayPointCmdReached = True
 		return wb_data
 
